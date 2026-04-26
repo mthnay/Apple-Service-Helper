98a4483d3a8f83837f8db5e6806f5b20f728f62e
Metehan Ay <mthnay@outlook.com>
2026-04-26 13:22:53 +0300

    .

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
+  server: {
+    proxy: {
+      '/api': 'http://localhost:5000',
+      '/upload-attachment': 'http://localhost:5000',
+      '/check-attachment': 'http://localhost:5000',
+      '/delete-attachment': 'http://localhost:5000',
+      '/test-connection': 'http://localhost:5000',
+      '/send-email': 'http://localhost:5000'
+    }
+  }
 })
