051c1732cbfe7d5ceee212e3f4ecf91b3371065c
Metehan Ay <92635187+mthnay@users.noreply.github.com>
2026-04-29 15:51:31 +0300

    .

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ const getAuthData = () => JSON.parse(fs.readFileSync(AUTH_FILE, 'utf8'));
 const saveAuthData = (data) => fs.writeFileSync(AUTH_FILE, JSON.stringify(data, null, 2));
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
